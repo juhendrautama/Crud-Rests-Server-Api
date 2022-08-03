3f1de710e89e31e19931557cdd6c7655fb8eb4e8
juhendrautama <hendracungkryng@gmail.com>
2022-08-03 15:47:17 +0700

    setting selesai

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Rests-Server/';
+$config['base_url'] = 'http://localhost/Crud-Rests-Server-Api/';
 
 /*
 |--------------------------------------------------------------------------
